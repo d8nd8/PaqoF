7ce0c9895f216e61cb15e702af5b4fe49a7d525a
RaysAndrey <andrew.rais@sportyfinder.ru>
2025-10-02 02:57:46 +0300

    fix: fix styles

--- src/shared/ui/fullscreen-modal/FullscreenModal.tsx
+++ src/shared/ui/fullscreen-modal/FullscreenModal.tsx
@@ -26,7 +26,7 @@ export const FullscreenModal: React.FC<FullscreenModalProps> = ({
     <S.Overlay $top={top} $bottom={bottom}>
       <S.ModalContent>
         <PageHeader
-          customTopInset={fullscreen ? 60 : top || 0}
+          customTopInset={fullscreen ? 80 : top || 0}
           title={title}
           onBack={onClose}
         />
