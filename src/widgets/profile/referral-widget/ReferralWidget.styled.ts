454359539fec90aeecb923ae8159bd2b97f889b4
RaysAndrey <andrew.rais@sportyfinder.ru>
2025-10-02 16:28:24 +0300

    fix: fix styles

--- src/widgets/profile/referral-widget/ReferralWidget.styled.ts
+++ src/widgets/profile/referral-widget/ReferralWidget.styled.ts
@@ -15,6 +15,7 @@ interface SwitchableContentProps {
 export const SwitchableContent = styled.div<SwitchableContentProps>(({ background }) => ({
   display: 'flex',
   flexDirection: 'column',
+  padding: '14px',
   background: background,
   borderBottomLeftRadius: '13px',
   borderBottomRightRadius: '13px',
