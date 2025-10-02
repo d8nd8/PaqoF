6c70c27bd95c380399b30a150655e52b61ba5341
RaysAndrey <andrew.rais@sportyfinder.ru>
2025-10-02 16:19:21 +0300

    fix: fix styles

--- src/features/overlay-wallet-deposit/WalletDepositOverlay.tsx
+++ src/features/overlay-wallet-deposit/WalletDepositOverlay.tsx
@@ -130,6 +130,7 @@ export const WalletDepositOverlay: React.FC<WalletDepositOverlayProps> = ({
     <>
       <S.OverlayWrapper>
         <PageHeader
+          customTopInset={fullscreen ? 80 : 0}
           title={
             title ||
             (mode === "deposit"
