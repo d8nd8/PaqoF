5ed9dbe465e906a85f05cdac9eafbadaf5beb16b
RaysAndrey <andrew.rais@sportyfinder.ru>
2025-10-14 19:39:54 +0300

    fix: fix styles

--- src/features/qr-scanner/QrScanner.styled.ts
+++ src/features/qr-scanner/QrScanner.styled.ts
@@ -81,7 +81,7 @@ export const CloseButton = styled.button<{ $insetTop?: number }>(({ $insetTop = 
   width: 44,
   height: 44,
   right: 16,
-  top: `calc(${ $insetTop }px + 20px)`,
+  top: `calc(${ $insetTop }px + 12px)`,
   color: 'white',
   background: 'transparent',
   border: 'none',
