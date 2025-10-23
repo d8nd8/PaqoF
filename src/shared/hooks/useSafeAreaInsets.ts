21861c004ea5c87c11aad7bd2ac54fcc73f0d122
RaysAndrey <andrew.rais@sportyfinder.ru>
2025-10-23 17:27:02 +0300

    fix: fix styles

--- src/shared/hooks/useSafeAreaInsets.ts
+++ src/shared/hooks/useSafeAreaInsets.ts
@@ -7,25 +7,10 @@ export function useSafeAreaInsets() {
   useEffect(() => {
     if (!viewport.safeAreaInsets) return;
 
-    const updateInsets = () => {
-      const safe = viewport.safeAreaInsets();
-      const isFullscreen = window.Telegram?.WebApp?.isExpanded ?? false;
-
-      setInsets({
-        top: isFullscreen ? safe.top || 20 : safe.top,
-        bottom: isFullscreen ? safe.bottom || 20 : safe.bottom,
-      });
-    };
-
-    updateInsets();
+    setInsets(viewport.safeAreaInsets());
 
     const unsubscribe = viewport.safeAreaInsets.sub((value) => {
-      const isFullscreen = window.Telegram?.WebApp?.isExpanded ?? false;
-
-      setInsets({
-        top: isFullscreen ? value.top || 20 : value.top,
-        bottom: isFullscreen ? value.bottom || 20 : value.bottom,
-      });
+      setInsets(value);
     });
 
     return unsubscribe;
