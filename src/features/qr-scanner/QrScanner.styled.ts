38184be18003228dd67d306f183be7d97874e6a6
RaysAndreyRU <raysandreyru@gmail.com>
2025-11-03 16:50:37 -0800

    fix: styles

--- src/features/qr-scanner/QrScanner.styled.ts
+++ src/features/qr-scanner/QrScanner.styled.ts
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 export const Overlay = styled.div({
   position: 'fixed',
   inset: 0,
-  backgroundColor: 'black',
+  backgroundColor: 'transparent',
   zIndex: 999,
   display: 'flex',
   flexDirection: 'column',
