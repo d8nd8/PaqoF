20b79dbec14160a8b409b2287b5caee2e6aad64a
Dreymandinn <andrei200277@gmail.com>
2026-02-12 13:35:22 +0300

    fix: bug fixes

--- src/widgets/welcome-slider/WelcomeSlider.tsx
+++ src/widgets/welcome-slider/WelcomeSlider.tsx
@@ -52,7 +52,7 @@ export const WelcomeSlider: React.FC = () => {
   }
 
   return (
-    <WelcomeSliderWrapper>
+    <WelcomeSliderWrapper style={{ paddingBottom: isLangOpen ? '60px' : 0 }}>
       <LangButton onClick={handleOpenLang}>
         <GlobeIcon />
       </LangButton>
